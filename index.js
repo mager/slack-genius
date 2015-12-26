1475d248a4b84075f3f864957aa7a60834503da0
Andrew Mager <andrew+github@mager.co>
2015-12-26 11:28:08 -0800

    Better text

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/post', function(req, res){
 
       body = {
         response_type: "in_channel",
-        text: "Here is a hardcoded result (test): " + first_url
+        text: first_url
       };
       res.send(body);
 
